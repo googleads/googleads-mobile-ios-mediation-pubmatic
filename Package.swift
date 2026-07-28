c8983fbc97d3dfb67371ae74c72fa68882870b6d
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-07-27 17:33:33 -0700

    Updating PubMatic iOS adapter for release version 5.2.0.
    PiperOrigin-RevId: 954922389

--- Package.swift
+++ Package.swift
@@ -28,11 +28,11 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/PubMatic/OpenWrapSDK-Swift-Package.git",
-      exact: "5.1.1"
+      exact: "5.2.0"
     ),
     .package(
       url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
-      from: "13.3.0"
+      from: "13.6.0"
     ),
   ],
   targets: [
@@ -48,7 +48,7 @@ let package = Package(
     .binaryTarget(
       name: "PubMaticAdapter",
       url:
-        "https://dl.google.com/googleadmobadssdk/mediation/ios/PubMatic/PubMaticAdapter-5.1.1.0.zip",
+        "https://dl.google.com/googleadmobadssdk/mediation/ios/PubMatic/PubMaticAdapter-5.2.0.0.zip",
       checksum: "30d12b8bad4eba14856efacb2075b556dd4e88280174667a21e1f116669402b0"
     ),
   ]
